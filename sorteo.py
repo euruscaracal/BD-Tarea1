0c482d39f1d99e5e67a3c1870e7b4879294137b0
Romina Caracal <tricoleromina@ciencias.unam.mx>
2019-08-22 23:38:36 -0500

    Dando formato al esqueleto, subiendo línea del tiempo

---
+++ sorteo.py
@@ -0,0 +1,17 @@
+from random import randrange, choice
+
+
+
+
+for contador in range(1,4, 1):
+    print(contador)
+    print(choice(['Teresa','Nicole','Miguel']))
+
+
+print('Por lo tanto el numero 1 hara incisos del ejercicio 1. a,d,g y j')
+print('Por lo tanto el numero 2 hara incisos del ejercicio 1. b,e,h')
+print('Por lo tanto el numero 3 hara incisos del ejercicio 1. c,f,i')
+
+print('Por lo tanto numero 1 hara del 2, el inciso a)')
+print('Por lo tanto numero 2 hara del 2 el inciso b)')
+print('Por lo tanto numero 3 hara del 2, el inciso c)')
